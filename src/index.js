517488f6516f5e4375c3f7265f44735e5c8d2050
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-09 18:22:36 -0500

    Adding component property value solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const numbers = Array.from(Array(5).keys());
+
+function NumberItems(props) {
+
+    let numbers = props.number;
+    let items = numbers.map(x => <li>{x}</li>)
+
+    return <ul>{items}</ul>
+}
+
+ReactDOM.render(<NumberItems number={numbers}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
